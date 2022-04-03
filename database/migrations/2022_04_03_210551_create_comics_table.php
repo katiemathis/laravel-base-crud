f2ecb76426547e71537a5680e6dae977c5babcd6
katiepalumbo <palumbo.katie@gmail.com>
2022-04-03 23:18:42 +0200

    migration created

---
+++ database/migrations/2022_04_03_210551_create_comics_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 255);
+            $table->text('description');
+            $table->string('thumb', 255);
+            $table->decimal('price', $precision = 8, $scale = 2);;
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 20);
+
+
+
+
+
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
