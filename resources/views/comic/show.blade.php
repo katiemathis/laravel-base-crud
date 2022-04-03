7943bc6cd9d95b9af9d7d6f4498780ba38255b7f
katiepalumbo <palumbo.katie@gmail.com>
2022-04-04 01:33:21 +0200

    added img to comic card

--- resources/views/comic/show.blade.php
+++ resources/views/comic/show.blade.php
@@ -8,7 +8,8 @@
 
     <div class="container">
             <h1>{{$comic->title}}</h1>
-            <div class="card" style="width: 50rem;">
+            <div class="card m-5" style="width: 20rem;">
+                <img src="{{$comic->thumb}}" class="card-img-top" alt="{{$comic->title}} Cover Photo">
                 <div class="card-body">
                 <h5 class="card-title">{{$comic->title}}</h5>
                 <p class="card-text">{{$comic->description}}</p>
@@ -18,7 +19,7 @@
                 <li class="list-group-item">Series: {{$comic->series}}</li>
                 </ul>
             </div>
-            <a type="button" class="btn btn-primary mt-5" href="{{route('comic.index')}}" role="button">Go Back</a>
+            <a type="button" class="btn btn-primary" href="{{route('comic.index')}}" role="button">Go Back</a>
     </div>
 
 @endsection
